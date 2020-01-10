6a1e48b62ca6bad55a20d1fbb7e6ec9275da79b4
hsiyu1121 <49058326+hsiyu1121@users.noreply.github.com>
2020-01-10 11:06:30 +0800

    Day026_HW

---
+++ homework/PTTCrawler.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class PttcrawlerSpider(scrapy.Spider):
+    name = 'PTTCrawler'
+    allowed_domains = ['www.ptt.cc']
+    start_urls = ['https://www.ptt.cc/bbs/Stock/index.html']
+    cookies = {'over18': '1'}
+
+    def parse(self, response):
+        pass
